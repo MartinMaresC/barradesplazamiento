09e734ce05343d7aa125604da9d614ea3645023e
Martin Mares <78748150+MartinMaresC@users.noreply.github.com>
2024-03-01 16:32:21 -0600

    App.js
    Barra de desplazamiento y funciones creadas para detenerse al final del scroll

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+
+const ScrollEffectComponent = () => {
+  const [scrollY, setScrollY] = useState(0);
+
+  useEffect(() => {
+    const barra = document.getElementById("barra");
+    const barra2 = document.getElementById("barra2");
+    const pixeles = document.getElementById("pixeles");
+    const maxScroll = document.documentElement.scrollHeight - window.innerHeight;
+
+
+    const updateBarPositions = () => {
+      const newScrollY = window.scrollY;
+
+      if (newScrollY <= maxScroll) {
+        barra.style.marginTop = `${newScrollY * 2}px`;
+      }
+
+      pixeles.innerHTML = newScrollY;
+      setScrollY(newScrollY);
+    };
+
+    document.addEventListener("scroll", updateBarPositions);
+
+    // Cleanup the event listener on component unmount
+    return () => {
+      document.removeEventListener("scroll", updateBarPositions);
+    };
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []); // Empty dependency array means this effect runs once, similar to componentDidMount
+
+  return (
+    <div>
+      <p className="barra" id="barra"></p>
+      <p className="barra2" id="barra2"></p>
+      <p id="pixeles"></p>
+    </div>
+  );
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ScrollEffectComponent />
     </div>
   );
 }
 
 export default App;
+
